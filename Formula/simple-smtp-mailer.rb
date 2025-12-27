4458340fe7e83effdeea89c6b8555de6868558c5
Brandon L. Burns <brandon@blburns.com>
2025-12-26 22:53:12 -0600

    rewrote formula for simple-smtp-mailer.rb with better dependency support

--- Formula/simple-smtp-mailer.rb
+++ Formula/simple-smtp-mailer.rb
@@ -8,7 +8,42 @@
 	# Dependencies
 	depends_on "openssl@1.1" => :build   # Build dependency for OpenSSL
 	depends_on "cmake" => :build
-	depends_on "make" => :build
+	depends_on "libpng" => :optional      # Optional runtime dependency
+	depends_on "git" => :recommended      # Recommending Git for version control (could be optional)
+  
+	# For handling multiple versions:
+	def install
+	  # Create directories for binaries, libraries, etc.
+	  bin.mkpath
+  
+	  # If you need to handle multiple versions and build from source:
+	  if version == "v0.1.0"
+		# Steps for building v1.2.0
+		# system "./configure", "--prefix=#{prefix}", "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}"
+		system "make clean &&make install"
+	  elsif version == "v0.2.0"
+		# Steps for building v1.1.0 (if needed)
+		# system "./configure", "--prefix=#{prefix}", "--disable-feature-x"
+		system "make clean && make install"
+	  else
+		# Default behavior if version is not specified
+		raise "Unsupported version: #{version}"
+	  end
+  
+	  # Optional post-install actions (linking, symlinking, etc.):
+	  ln_s bin/"simple-smtp-mailer", bin/"simple-smtp-mailer" if version == "v0.2.0"
+  
+	  # Removing any unnecessary files after installation
+	  rm_f Dir["#{lib}/simple-smtp-mailer/*.a"]
+	end
+  
+	# Handle version conflicts:
+	def caveats
+	  <<~EOS
+		If you want to switch versions, you can install the desired version manually:
+		  brew install blburns/tap/simple-smtp-mailer@0.1.0
+	  EOS
+	end
   
 	# Version support - install older versions if needed
 	def self.latest_version
@@ -17,8 +52,8 @@
   
 	# Installation steps for other versions or handling more complex logic
 	def post_install
-	  if version == "v0.2.0"
-		ohai "Simple SMTP Mailer version 0.2.0 has been installed!"
+	  if version == "v0.1.0"
+		ohai "Simple SMTP Mailer version 0.1.0 has been installed!"
 		# Perform any extra setup for this version
 	  end
 	end
@@ -27,7 +62,7 @@
 	test do
 	  # You can write a small test to check if your app is installed properly
 	  system "#{bin}/simple-smtp-mailer", "--version"
-	  assert_match "simple-smtp-mailer version 0.2.0", shell_output("#{bin}/simple-smtp-mailer --version")
+	  assert_match "simple-smtp-mailer version v0.2.0", shell_output("#{bin}/simple-smtp-mailer --version")
 	end
   end
   
